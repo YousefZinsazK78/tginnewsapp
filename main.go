cbf47cd26c4a4d4a16aa659fc337964fad09c332
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-09-09 13:04:01 +0330

    Add gin-gonic, godotenv packages to prepare our project

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("hello, world!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"some-message-key": "this is awesome , we did it !😁😁",
+		})
+	})
+	router.Run(":" + os.Getenv("PORT"))
 }
